d2653b5a032230f4403e99557a726126e4ed9e2f
Peri <periasamy.kanagavel@outlook.com>
2025-06-18 20:19:09 +0530

    fix for using iterator

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ arr[Symbol.iterator] = function () {
     return {
         next: function() {
             if (i < arr.length) {
-                const value = arr[i++];
-                i++;
+                let value = arr[i];
+                i++; // skips one element (every second one)
                 return { value , done: false };  
             } else {
                 return { done: true };
@@ -16,16 +16,12 @@ arr[Symbol.iterator] = function () {
     };
 }
 
-console.log(arr[Symbol.iterator]().next()); // { value: 'a', done: false }
-console.log(arr[Symbol.iterator]().next()); // { value: 'c', done: false }
-console.log(arr[Symbol.iterator]().next()); // { value: 'e', done: false }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }
-console.log(arr[Symbol.iterator]().next()); // { done: true }  
+// Store the iterator in a variable
+const it = arr[Symbol.iterator]();
+// Use the iterator to get values
+console.log(it.next()); // { value: 'a', done: false }
+console.log(it.next()); // { value: 'b', done: false }
+console.log(it.next()); // { value: 'c', done: false }
+console.log(it.next()); // { value: 'd', done: false  }
+console.log(it.next()); // { value: 'e', done: false  }
+console.log(it.next()); // { done: true }
